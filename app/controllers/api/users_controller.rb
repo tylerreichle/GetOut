c060fa95332e60593efa8e0097b1d4ec9c5d2cfe
jilldela <jilldelosangeles@gmail.com>
2017-06-05 11:06:59 -0700

    Create users controller

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,25 @@
+class Api::UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = current_user
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login(@user)
+      render "api/users/show"
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password, :email)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api, defaults: { format: :json } do
+    resources :user, only: [:index, :show, :create]
+  end
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

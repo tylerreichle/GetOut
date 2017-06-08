0927b23bcc0382119c9b187c3f59e04e416cc8a5
Tyler <tyler.reichle@gmail.com>
2017-06-07 18:15:36 -0700

    Fix second login issue

--- mobile/src/components/session/LoginForm.js
+++ mobile/src/components/session/LoginForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, TextInput, Text, Button } from 'react-native';
 import { Actions } from 'react-native-router-flux';
+import { AsyncStorage } from 'react-native';
 
 export default class LoginForm extends Component {
 
@@ -15,7 +16,7 @@ export default class LoginForm extends Component {
   }
 
   componentWillReceiveProps(newProps) {
-    if(!Array.isArray(newProps.currentUser)) {
+    if (!Array.isArray(newProps.currentUser)) {
       Actions.categoriesIndex();
     }
   }
@@ -23,6 +24,10 @@ export default class LoginForm extends Component {
   onButtonSubmit() {
     const { username, password } = this.state;
     this.props.login({ username, password });
+
+    setTimeout(() => {
+      this.getToken();
+    }, 400);
   }
 
   handleChange(value, name) {
@@ -36,16 +41,50 @@ export default class LoginForm extends Component {
       return (
         <Text
           style={{
-          textAlign: 'center',
-          fontSize: 20,
-          color: '#cc3333'
-        }}
+            textAlign: 'center',
+            fontSize: 20,
+            color: '#cc3333'
+          }}
         >Sorry authentication failed!</Text>
       );
     }
     return null;
   }
 
+  async getToken() {
+    try {
+      let sessionToken = await AsyncStorage.getItem('sessionToken');
+
+      if (!sessionToken) {
+        console.log("Token not set");
+      } else {
+        this.verifyToken(sessionToken)
+      }
+    } catch (error) {
+      console.log("Error finding token");
+    }
+  }
+
+  async verifyToken(token) {
+    const sessionToken = token;
+
+    try {
+      let response = await fetch('http://localhost:3000/api/verify?session%5Bsession_token%5D=' + sessionToken);
+      let res = await response.text();
+      if (response.status >= 200 && response.status < 300) {
+        //Verified token means user is logged in so we redirect them home.
+        console.log('user still logged in');
+
+        Actions.categoriesIndex();
+      } else {
+        //Handle error
+        const error = res;
+        throw error;
+      }
+    } catch (error) {
+      console.log("error response: " + error);
+    }
+  }
 
   render() {
     return (
@@ -57,7 +96,7 @@ export default class LoginForm extends Component {
         justifyContent: 'center',
         alignItems: 'center'
       }}
-      linkAction={ Actions.loginForm }
+        linkAction={Actions.loginForm}
       >
         <TextInput
           style={{
@@ -74,7 +113,7 @@ export default class LoginForm extends Component {
           autoCapitalize="none"
           placeholder={'Username'}
           value={this.state.username}
-          onChangeText={(value) => this.handleChange(value , 'username')}
+          onChangeText={(value) => this.handleChange(value, 'username')}
         />
 
         <TextInput
@@ -93,9 +132,9 @@ export default class LoginForm extends Component {
           secureTextEntry
         />
         <View
-          style={{backgroundColor: 'green', width: 150}}>
+          style={{ backgroundColor: 'green', width: 150 }}>
           <Button
-            color= 'white'
+            color='white'
             title="Login"
             onPress={this.onButtonSubmit.bind(this)}
           >

559b4a1cd853224ad6ad3b5c277de9aafc9c193d
Tyler <tyler.reichle@gmail.com>
2017-06-08 09:50:42 -0700

    Finish message and chatroom controllers

--- app/controllers/api/chatrooms_controller.rb
+++ app/controllers/api/chatrooms_controller.rb
@@ -4,6 +4,15 @@ class Api::ChatroomsController < ApplicationController
   end
 
   def create
+    @chatroom = Chatroom.new(chatroom_params)
+    @chatroom.user_id = current_user.id
+
+    if @chatroom.save
+      # subscribe to chat
+      render 'api/chatrooms/show'
+    else
+      render json: @chatroom.errors.full_messages
+    end
     
   end
 
@@ -11,20 +20,27 @@ class Api::ChatroomsController < ApplicationController
     @chatroom = Chatroom.find_by(id: params[:id])
     @message = Message.new
     
-    if @chatroom.user_id === current_user.id
+    if @chatroom.user_id == current_user.id
       render 'api/chatrooms/show'
     else
-      render json: 'Cannot view another person\'s chat', status: 422
+      render json: 'Cannot view another person\'s chat', status: 403
     end
   end
   
   def delete
+    @chatroom = Chatroom.find_by(id: params[:id])
+
+    if @chatroom.user_id == current_user.id 
+      @chatroom.delete
+    else
+      render json: 'Cannot remove another person\'s chat', status: 403
+    end
   end
 
   private
 
   def chatroom_params
-    params.require(:chatroom).permit(:message_id, )
+    params.require(:chatroom).permit(:message_id, :user_id)
   end
   
 end
--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,7 +1,21 @@
 class Api::MessagesController < ApplicationController
   def create
+    @message = Message.new(message_params)
+    @message.user_id = current_user.id
+
+    if @message.save
+      # send message
+    else
+      render json: @message.errors.full_messages
+    end
   end
 
   def delete
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body, :chatroom_id)
   end
 end

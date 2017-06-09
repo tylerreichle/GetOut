e0ef793d737876fae815f61d93fd3028e8cd8186
eadams17 <erictayloradams@gmail.com>
2017-06-08 20:48:59 -0700

    Render form errors

--- mobile/src/reducers/error_reducer.js
+++ mobile/src/reducers/error_reducer.js
@@ -1,10 +1,12 @@
-import { RECEIVE_ERRORS } from '../actions/error_actions';
+import { RECEIVE_ERRORS, CLEAR_ERRORS } from '../actions/error_actions';
 
 const errorReducer = ( state = [], action ) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_ERRORS:
       return [...action.errors];
+    case CLEAR_ERRORS:
+      return [];
     default:
       return state;
   }

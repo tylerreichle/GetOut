ed5a239b19ecbabe8fb2111af00de322b202e872
Tyler <tyler.reichle@gmail.com>
2017-07-08 19:36:46 -0700

    Rename and clean up Routes

--- mobile/src/Routes.jsx
+++ mobile/src/Routes.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Scene, Router} from 'react-native-router-flux';
+import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/session/LoginFormContainer';
 import SignUpForm from './components/session/SignUpFormContainer';
 import CategoriesIndex from './components/categories/categories_container';
@@ -13,66 +13,66 @@ import Profile from './components/user_profile/profile_container';
 const Routes = () => (
   <Router sceneStyle={{ backgroundColor: 'white' }}>
     <Scene
-      key='splash'
-      component={ Splash }
+      key="splash"
+      component={Splash}
       title="Welcome"
-      hideNavBar={true}
+      hideNavBar
       initial
     />
 
     <Scene
-      key='loginForm'
-      component={ LoginForm }
-      title='Login'
+      key="loginForm"
+      component={LoginForm}
+      title="Login"
       hideNavBar={false}
     />
 
     <Scene
-      key='signupForm'
-      component={ SignUpForm }
-      title='Sign Up'
+      key="signupForm"
+      component={SignUpForm}
+      title="Sign Up"
       hideNavBar={false}
     />
 
     <Scene
-      key='categoriesIndex'
-      component={ CategoriesIndex }
-      title='Categories Index'
-      hideNavBar={true}
+      key="categoriesIndex"
+      component={CategoriesIndex}
+      title="Categories Index"
+      hideNavBar
     />
 
     <Scene
-      key='CategoriesIndexItem'
-      component={ CategoriesIndexItem }
-      title='Similar interests...'
+      key="CategoriesIndexItem"
+      component={CategoriesIndexItem}
+      title="Similar interests..."
       hideNavBar={false}
     />
 
     <Scene
-      key='Profile'
-      component={ Profile }
-      title='Profile'
+      key="Profile"
+      component={Profile}
+      title="Profile"
       hideNavBar={false}
     />
 
     <Scene
-      key='Dashboard'
-      component={ Dashboard }
-      title='Dashboard'
-      hideNavBar={true}
+      key="Dashboard"
+      component={Dashboard}
+      title="Dashboard"
+      hideNavBar
     />
 
     <Scene
-      key='ChatroomIndex'
-      component={ ChatroomIndex }
-      title='Messages'
+      key="ChatroomIndex"
+      component={ChatroomIndex}
+      title="Messages"
       hideNavBar={false}
     />
 
     <Scene
-      key='ChatroomShow'
-      component={ ChatroomShow }
-      title='Chat'
+      key="ChatroomShow"
+      component={ChatroomShow}
+      title="Chat"
       hideNavBar={false}
     />
   </Router>

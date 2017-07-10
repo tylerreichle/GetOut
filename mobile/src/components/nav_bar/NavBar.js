fe5fbcaf9618d0e02cefefd236adfd676b1c8e3c
Tyler <tyler.reichle@gmail.com>
2017-07-09 23:11:20 -0700

    Update NavBar style

--- mobile/src/components/nav_bar/NavBar.js
+++ mobile/src/components/nav_bar/NavBar.js
@@ -1,50 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Tabs from 'react-native-tabs';
 import PropTypes from 'prop-types';
 import { Actions } from 'react-native-router-flux';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class NavBar extends Component {
-  constructor(props) {
-    super(props);
+const NavBar = props => (
+  <View style={styles.container}>
+    <Tabs
+      selected={''}
+      style={{ backgroundColor: 'grey' }}
+      selectedStyle={{}}
+      selectedIconStyle={{}}
+    >
 
-    this.state = { page: 'CategoriesIndex' };
-  }
+      <Text
+        name="CategoriesIndex"
+        onPress={() => { Actions.CategoriesIndex(); }}
+        style={styles.button}
+      >Interests</Text>
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Tabs
-          selected={this.state.page}
-          style={{ backgroundColor: 'grey' }}
-          selectedStyle={{ color: 'black' }}
-          selectedIconStyle={{ borderTopWidth: 2, borderTopColor: '#26628c' }}
-          onSelect={(el) => { this.setState({ page: el.props.name }); }}
-        >
+      <Text
+        name="ChatroomIndex"
+        onPress={() => { Actions.ChatroomIndex(); }}
+        style={styles.button}
+      >Messages</Text>
 
-          <Text
-            name="CategoriesIndex"
-            onPress={() => Actions.CategoriesIndex()}
-            style={styles.button}
-          >Home</Text>
+      <Text
+        name="Logout"
+        onPress={props.logout}
+        style={styles.button}
+      >Log Out</Text>
 
-          <Text
-            name="ChatroomIndex"
-            onPress={() => Actions.ChatroomIndex()}
-            style={styles.button}
-          >Messages</Text>
-
-          <Text
-            name="Logout"
-            onPress={this.props.logout}
-            style={styles.button}
-          >Log Out</Text>
-
-        </Tabs>
-      </View>
-    );
-  }
-}
+    </Tabs>
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
@@ -54,7 +43,7 @@ const styles = StyleSheet.create({
   button: {
     margin: 10,
     color: 'white',
-    fontSize: 16,
+    fontSize: 20,
     textAlign: 'center',
   },
 });
@@ -62,3 +51,5 @@ const styles = StyleSheet.create({
 NavBar.propTypes = {
   logout: PropTypes.func.isRequired,
 };
+
+export default NavBar;
